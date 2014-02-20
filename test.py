9b01822c4fd874e7a0643492383ed680a87351fe
= <=>
2014-02-19 20:09:59 -0600

    closed #6 - overlay ability for debug mode

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-name = 'view'
-cv2.namedWindow( name, cv2.CV_WINDOW_AUTOSIZE )
-img = cv2.imread('screenshot.png')
-
-cv2.circle(img, (100,100), 3, [255, 0,0], -1)
-cv2.imshow(name, img)
-cv2.waitKey()
